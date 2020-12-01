61a17dc8e92b0ab3cdc4a5356efef61f1d868410
Your Name <jgbharris@hotmail.co.uk>
2020-12-01 01:30:18 +0000

    removing console error

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,8 @@
-import { propTypes } from "react-json-pretty";
 
 export default function Header (props) {
 
     return (
-        <div class="header" id="myHeader">
+        <div className="header" id="myHeader">
             <h2>{props.title}</h2>
         </div>
 
